90da83de24c46c0cf3b9bae394b36a02970f1dec
Natalia <nat.lpzgl@gmail.com>
2023-11-05 11:24:19 +0100

    commit prueba

---
+++ app/src/main/java/com/ifp/green_market/ActivityPrueba.java
@@ -0,0 +1,14 @@
+package com.ifp.green_market;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityPrueba extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prueba);
+    }
+}
