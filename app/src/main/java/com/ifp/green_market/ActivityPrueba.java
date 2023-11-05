23289a0643871c46c4c9cc58a09dbca0ac7aa488
Andrea Morán <andreamgajate@gmail.com>
2023-11-05 11:37:36 +0100

    Holi me quiero morir

--- app/src/main/java/com/ifp/green_market/ActivityPrueba.java
+++ app/src/main/java/com/ifp/green_market/ActivityPrueba.java
@@ -10,5 +10,6 @@ public class ActivityPrueba extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prueba);
+        //Hola
     }
 }
